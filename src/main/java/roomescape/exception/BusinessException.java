f69419dd3589c7bdf0bbbd1ab995de340ba50558
HeeJoon-Kwon <2390kwon@g.skku.edu>
2026-05-14 13:22:20 +0900

    refactor: ErrorResponseBody

--- src/main/java/roomescape/exception/BusinessException.java
+++ src/main/java/roomescape/exception/BusinessException.java
@@ -1,11 +1,6 @@
 package roomescape.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 public class BusinessException extends RuntimeException {
     public BusinessException(String message, HttpStatus status) {
@@ -17,13 +12,5 @@ public class BusinessException extends RuntimeException {
 
     public HttpStatus getStatus() {
         return status;
-    }
-
-    public ResponseEntity<Map<String, Object>> createExceptionResponse() {
-        Map<String, Object> body = new HashMap<>();
-        body.put("message", getMessage());
-        body.put("timestamp", LocalDateTime.now());
-
-        return new ResponseEntity<>(body, getStatus());
     }
 }
