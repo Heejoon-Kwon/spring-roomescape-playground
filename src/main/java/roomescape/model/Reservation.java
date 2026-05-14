277eba4c776f756b94ffb3794adc1622f0b56b79
HeeJoon-Kwon <2390kwon@g.skku.edu>
2026-05-14 22:08:54 +0900

    feat: H2 database attached

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -1,5 +1,6 @@
 package roomescape.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Reservation {
 
     public static final int AVAILABLE_HOURS = 2;
@@ -19,9 +21,17 @@ public class Reservation {
 
     private LocalDateTime endTime;
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    public Reservation() {}
+
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setDateAndTime(LocalDate date, LocalTime time) {
         this.startTime = LocalDateTime.of(date, time);
         this.endTime = startTime.plusHours(AVAILABLE_HOURS);
     }
--- src/main/java/roomescape/repository/UpdatingDAO.java
+++ src/main/java/roomescape/repository/UpdatingDAO.java
@@ -1,9 +1,12 @@
 package roomescape.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.model.Reservation;
+
+import java.sql.PreparedStatement;
 
 @Repository
 public class UpdatingDAO {
@@ -22,5 +25,26 @@ public class UpdatingDAO {
                 reservation.getStartTime().toLocalDate(),
                 reservation.getStartTime().toLocalTime()
         );
+    }
+
+    public Long insertWithKeyHolder(Reservation reservation) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "insert into reservation (name, date, time) values (?, ?, ?)",
+                    new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setObject(2, reservation.getStartTime().toLocalDate());
+            ps.setObject(3, reservation.getStartTime().toLocalTime());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public int delete(Reservation reservation) {
+        String sql = "delete from reservation where id = ?";
+        Long id = reservation.getId();
+        return jdbcTemplate.update(sql, id);
     }
 }
